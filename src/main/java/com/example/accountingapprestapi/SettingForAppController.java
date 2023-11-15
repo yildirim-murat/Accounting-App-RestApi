4b1c608153ff1afd505965c63064bdec12f66460
yildirim-murat <muratyildirim.work@gmail.com>
2023-11-15 15:07:14 +0300

    Checked random number

--- src/main/java/com/example/accountingapprestapi/SettingForAppController.java
+++ src/main/java/com/example/accountingapprestapi/SettingForAppController.java
@@ -7,7 +7,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class SettingForAppController {
     @GetMapping
     public String sayHello(){
-        return "Hi!Test!";
+        return "Hi!Test!2";
+    }
+
+    @GetMapping("/random")
+    public String getNumber(){
+        double value = Math.random()*100;
+        return "Created Number: " + value;
     }
     @GetMapping("/sentence")
     public String getSentence(){
